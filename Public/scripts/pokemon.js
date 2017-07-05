4781fe3326d4ba4b9eb39497a4396643ef9c0b47
Allan Liebold <allan.liebold@gmail.com>
2017-07-05 11:14:06 -0700

    adds base stats to pokedex entries

--- Public/scripts/pokemon.js
+++ Public/scripts/pokemon.js
@@ -4,14 +4,23 @@ function Pokemon(data){
   this.id = data.id;
   this.name = data.name.charAt(0).toUpperCase() + data.name.slice(1);
   this.sprite = data.sprites.front_default;
-  this.type = data.types[0].type.name;
 
+// Pokemon's type. If a Pokemon is not dual type, secondType is an empty string.
+  this.type = data.types[0].type.name;
   if(data.types.length > 1) {
     this.secondType = data.types[1].type.name;
   } else {
     this.secondType = '';
   }
-  // this.typeFilter = data.type[0];
+
+// Pokemon's base stats
+  this.speed = data.stats[0].base_stat;
+  this.spDef = data.stats[1].base_stat;
+  this.spAtk = data.stats[2].base_stat;
+  this.def = data.stats[3].base_stat;
+  this.atk = data.stats[4].base_stat;
+  this.hp = data.stats[5].base_stat;
+
 }
 
 var pokedex = [];

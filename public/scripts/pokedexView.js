579af4fb929e46c30d7cfc68ec55947b35f65789
Allan Liebold <allan.liebold@gmail.com>
2017-07-06 13:17:03 -0700

    can select pokemon by generation

--- public/scripts/pokedexView.js
+++ public/scripts/pokedexView.js
@@ -3,32 +3,12 @@
 const pokedexView = {};
 
 pokedexView.handleGenFilter = function() {
-  var firstGen;
-  // firstGen = 1 - 151
-  var secondGen;
-  // secondGen = 152 - 251
-  var thirdGen;
-  // thirdGen = 252 - 386
-  var fourthGen;
-  // fourthGen = 387 - 493
-  var fifthGen;
-  // fifthGen = 494 - 649
-  var sixthGen;
-  // sixthGen = 650 - 721
-  var seventhGen;
-  // seventhGen = 722 - 802
-
   $('#gen-filter').on('change', function() {
-    if($(this).val() === 'first-gen') {
-      $('.dex-entry').hide();
-      $(`.dex-entry[data-id="${firstGen}"]`).fadeIn();
-    }
-    if($(this).val() === 'second-gen'){
-      $('.dex-entry').hide();
-      $(`.dex-entry[data-id="${secondGen}"]`).fadeIn();
-    }
-    if($(this).val() === ''){
-      $('.dex-entry').fadeIn();
+    if($(this).val()) {
+      $('.list-name').hide();
+      $(`.list-name[data-gen="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('.list-name').fadeIn();
     }
   })
 }
